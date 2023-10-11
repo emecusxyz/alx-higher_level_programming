98f467ce74040d6fd5b33d9003c63970b805649d
emeka nwobu <emecusxyz@yahoo.com>
2023-10-11 14:26:03 -0700

    corretion

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     """Replace or add key/value pairs in a dictionary."""
-    a_dictionary[key] = ivalue
+    a_dictionary[key] = value
     return (a_dictionary)
